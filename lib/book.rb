35e694c1e8e6513a3d2432672ee2080542ff7f2d
Alex Suthammanont <asuthamm@gmail.com>
2019-11-19 22:04:03 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -19,10 +19,19 @@ class Book
   def page_count=(page_count)
     @page_count = page_count
   end
+  def page_count
+    @page_count
+  end
   def genre=(genre)
     @genre = genre
   end
+  def genre
+    @genre
+  end
   def turn_page=(turn_page)
     @turn_page = turn_page
+  end
+  def turn_page
+    @turn_page
   end
 end
