75c0a5ffa373b89042d800eac48d4ed174034f3b
Alex Suthammanont <asuthamm@gmail.com>
2019-11-19 22:08:59 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,10 +1,6 @@
 class Book
   def initialize(title)
     @title = title
-    # @author = author
-    # @page_count = page_count
-    # @genre = genre
-    # @turn_page = turn_page
   end
   
   def title
@@ -28,10 +24,7 @@ class Book
   def genre
     @genre
   end
-  # def turn_page=(turn_page)
-  #   @turn_page = turn_page
-  #   puts "Flipping the page...wow, you read fast!"
-  # end
+  
   def turn_page
     puts "Flipping the page...wow, you read fast!"
   end
