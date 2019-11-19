62b710ef08a31612b73c8761846a6f0e0f05d850
Alex Suthammanont <asuthamm@gmail.com>
2019-11-19 21:57:50 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,10 +1,10 @@
 class Book
   def initialize(title)
     @title = title
-    @author = author
-    @page_count = page_count
-    @genre = genre
-    @turn_page = turn_page
+    # @author = author
+    # @page_count = page_count
+    # @genre = genre
+    # @turn_page = turn_page
   end
   
   def title
